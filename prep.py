a11e89199cfa5d8e2a85c349fe651353c0689e9e
John <johnruge@uchicago.edu>
2024-09-12 08:02:39 -0500

    more questions

--- prep.py
+++ prep.py
@@ -4,6 +4,7 @@ return true if there're three numbers that add up to the target and false if
 there is not.
 """
 
+#the best i could to is o(n^2)
 def three_sum(nums: list[int], target: int) -> bool:
     dict_nums = {}
     for i, v in enumerate(nums):
@@ -19,6 +20,7 @@ def three_sum(nums: list[int], target: int) -> bool:
 
     return False
 
+
 #2 two_sum(blind75)
 """
 Given an array of integer nums and an integer target, return
@@ -29,13 +31,21 @@ You can return the answer in any order.
 """
 
 # o(n^2) solution. this is the brute force solution. it is not efficient but uses less space.
-def two_sum(nums: list[int], target: int) -> list[int]:
+def two_sum_brute(nums: list[int], target: int) -> list[int]:
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
                 return [i, j]
 
 # o(n) solution. this is a more efficient solution that uses extra space creating a dictionary.
+def two_sum_eff(nums: list[int], target: int) -> list[int]:
+    dict_nums = {target - nums[0]: [nums[0], 0]}
+
+    for i in range(1, len(nums)):
+        if nums[i] in dict_nums:
+            return [dict_nums[nums[i]][1], i]
+        dict_nums[target - nums[i]] = [nums[i], i]
+
 
 #3 best time to buy and sell stock
 """
@@ -48,8 +58,51 @@ Return the maximum profit you can achieve from this transaction.
 If you cannot achieve any profit, return 0
 """
 
+#o(n). this solution is pretty efficient. i used the sliding window technique
+def max_profit_stocks(prices: list[int]) -> int:
+    min_price, max_profit = prices[0], 0
+
+    for i in range(1, len(prices)):
+        if prices[i] <= min_price:
+            min_price = prices[i]
+        elif prices[i] > min_price:
+            profit = prices[i] - min_price
+            max_profit = max(max_profit, profit)
+
+    return max_profit
+
+
+#4 contains duplicate
+"""
+Given an integer array nums, return true if any value appears at
+least twice in the array, and return false if every element is
+distinct.
+Input: nums = [1,2,3,1]
+Output: true
+"""
+
+#O(n). pretty efficient
+def contains_duplicate(nums: list[int]) -> bool:
+    set_nums = set()
+    for i in range(len(nums)):
+        if nums[i] in set_nums:
+            return True
+        set_nums.add(nums[i])
+
+    return False
+
+#product of array except self
+"""
+Given an integer array nums, return an array answer such that
+answer[i] is equal to the product of all the elements of nums
+except nums[I].
+The product of any prefix or suffix of nums is guaranteed to fit in a
+32-bit integer.
+You must write an algorithm that runs in O(n) time and without
+using the division operation.
+"""
 def main():
-    print(two_sum([0,7,3, 15, 4, 89, 5, 6, 123], 9))
+    print(contains_duplicate([100,1000, 30,10,4]))
     return None
 
 if __name__ == "__main__":
